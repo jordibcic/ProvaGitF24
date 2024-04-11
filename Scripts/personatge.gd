29d88ca071af8d75895160ad78af160c5db73f72
jordibcic <49191624+jordibcic@users.noreply.github.com>
2024-04-11 08:24:20 +0200

    Llums i autotile

--- Scripts/personatge.gd
+++ Scripts/personatge.gd
@@ -17,6 +17,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("ui_cancel"):
+		$PointLight2D.enabled = !$PointLight2D.enabled
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
